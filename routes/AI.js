9d49571e2389e1880175448e58d095a9eeacb75f
Elksass315 <qsasomar@gmail.com>
2024-05-10 22:10:29 +0300

    init plants router with disease route

---
+++ routes/AI.js
@@ -0,0 +1,28 @@
+const tf = require("@tensorflow/tfjs-node")
+const express = require("express");
+const router = express.Router();
+const { createCanvas, Image } = require('canvas');
+const fs = require('fs');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+
+router.post('/disease', upload.single('image') ,async (req, res) => {
+    let model = await tf.loadLayersModel('../model/AI-model/plant_model_v2.h5');
+    const img = new Image();
+    img.src = fs.readFileSync(req.file.path);
+
+    targetSize = [256, 256];
+    const canvas = createCanvas(targetSize[0], targetSize[1]);
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, targetSize[0], targetSize[1]);
+    const resizedImg = tf.browser.fromPixels(canvas);
+
+    const normalizedImg = resizedImg.toFloat().div(tf.scalar(255));
+    const processedImg = normalizedImg.expandDims();
+
+    const predictions = model.predict(processedImg);
+    predictions.print();
+
+    res.send(predictions);
+});
