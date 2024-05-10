d0b154a4cd07dcdb8fc7334683d053483e4b482d
Elksass315 <qsasomar@gmail.com>
2024-05-10 22:08:59 +0300

    init favorites router

---
+++ routes/favorites.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const Favorite = require("../model/favorites");
+const auth = require("../middleware/auth");
+
+router.put("/", auth, async (req, res) => {
+    const favorite = new Favorite({
+        User: req.user._id,
+        product: req.body.product
+    });
+    try {
+        await favorite.save();
+        res.send(favorite);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+router.get("/", auth, async (req, res) => {
+    const favorite = await Favorite.find({ User: req.user._id }).populate("product");
+    res.send(favorite);
+});
+
+router.delete("/:id", auth, async (req, res) => {
+    const favorite = await Favorite.findByIdAndDelete(req.params.id);
+    if (!favorite) return res.status(404).send("The favorite with the given ID was not found.");
+    res.send(favorite);
+});
+
+module.exports = router;
