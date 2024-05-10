940423528fb33a5e104ef238515325ae3eaa1177
Elksass315 <qsasomar@gmail.com>
2024-05-10 21:57:23 +0300

    init weather router

---
+++ routes/weather.js
@@ -0,0 +1,24 @@
+const c = require('config');
+const express = require('express');
+const route = express.Router();
+// const fetch = require('node-fetch');
+const config = require('config');
+
+route.get('/',async (req, res) => {
+    if(!req.body.city) {
+        return res.status(400).send('City is required');
+    }
+
+    if(!config.get('weatherApiKey')) {
+        return res.status(500);
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&appid=${config.get('weatherApiKey')}`;
+    console.log(url);
+    fetch(url).then(response => response.json()).then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send(err)
+    });
+});
+
+module.exports = route;
