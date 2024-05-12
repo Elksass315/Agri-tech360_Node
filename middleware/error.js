1a9a5b143ac9703962fda3abe2c011ba41af517b
Elksass315 <qsasomar@gmail.com>
2024-05-12 23:34:54 +0300

    Fix TypeError: Joi.validate

--- middleware/error.js
+++ middleware/error.js
@@ -2,5 +2,5 @@ const winston = require("winston");
 
 module.exports = function (error, req, res, next) {
     winston.error(error.message, error)
-    res.status(500),send("somthing faild")
+    res.status(500).send("somthing faild")
 }
--- routes/auth.js
+++ routes/auth.js
@@ -7,9 +7,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    // TODO: FIX TypeError: Joi.validate is not a function
-    // const { error } = validate(req.body);
-    // if (error) return res.status(400).send(error.details[0].message);
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
     let user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(400).send('Invalid email or password.');
@@ -22,15 +21,14 @@ router.post('/', async (req, res) => {
 });
 
 
-// TODO: FIX TypeError: Joi.validate is not a function
-
-// function validate(req) {
-//     const schema = {
-//         email: Joi.string().min(5).max(255).required().email(),
-//         password: Joi.string().min(5).max(255).required()
-//     };
-
-//     return Joi.validate(req, schema);
-// }
+function validate(req) {
+    const schema = Joi.object({
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+        password: Joi.string()
+            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+    });
+    return schema.validate(req);
+}
 
 module.exports = router; 
