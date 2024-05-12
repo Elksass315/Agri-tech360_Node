3165a53e2e0c131a41dd44358b853a59b0f5e382
Elksass315 <qsasomar@gmail.com>
2024-05-12 07:19:41 +0300

    Extracting config logic

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-const winston = require("winston")
 const express = require("express");
-const config = require("config");
 const app = express();
 
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/DB")();
+require("./startup/config")();
 
-if (!config.get("jwtPrivateKey")) {
-    console.error("FATAL ERROR: jwtPrivateKey is not defined.");
-    process.exit(1);
-}
 
 app.get("/", (req, res) => {
     res.send(`Welcome to Agri-Tech360`)
 })
 
-const port = 5000 || config.get("port");
+const port = 5000 || process.env.PORT;
 app.listen(port, () => {
     console.log(`Server start successfully at http://127.0.0.1:${port}`)
 })
