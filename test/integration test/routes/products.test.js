9b0c1d9f55d0a100dbed8e7925343dce73fe63e0
Elksass315 <qsasomar@gmail.com>
2024-05-18 11:32:46 +0300

    New: product route testing

---
+++ test/integration test/routes/products.test.js
@@ -0,0 +1,46 @@
+const request = require('supertest');
+const Product = require('../../../model/products');
+const User = require('../../../model/users');
+let server
+
+
+describe('/api/products', () => {
+    beforeEach(() => { server = require('../../../index') });
+    afterEach(async () => {
+        server.close(),
+        await Product.deleteMany({})
+        await User.deleteMany({})
+
+    });
+
+    describe('GET /', () => {
+        it('should return all products', async () => {
+            const user = await User.collection.insertOne({ fullName: 'user1', email: 'test1@test1.com', password: '123456',phoneNumber: '+234567890123', isAdmin: true});
+            await Product.collection.insertMany([
+                { name: 'product1', price: 100, description: 'product1 description', category: 'category1', image: 'image1', seller: user._id},
+                { name: 'product2', price: 100, description: 'product2 description', category: 'category2', image: 'image2', seller: user._id }
+            ]);
+
+            const res = await request(server).get('/api/product');
+            expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            expect(res.body.some(p => p.name === 'product1')).toBeTruthy();
+            expect(res.body.some(p => p.name === 'product2')).toBeTruthy();
+        });
+    });
+
+    describe('GET /:id', () => {
+        it('should return a product if valid id is passed', async () => {
+            const product = new Product({ name: 'product1', price: 100, description: 'product1 description', category: 'category1', image: 'image1', seller: user._id });
+            await product.save();
+            const res = await request(server).get('/api/product/' + product._id);
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('name', product.name);
+        });
+
+        it('should return 404 if invalid id is passed', async () => {
+            const res = await request(server).get('/api/product/1');
+            expect(res.status).toBe(404);
+        });
+    });
+});
