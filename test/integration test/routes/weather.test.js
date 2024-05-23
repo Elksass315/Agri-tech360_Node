d13f5d5152e94a182b60e18facd1eea881c99942
Elksass315 <qsasomar@gmail.com>
2024-05-23 11:48:04 +0300

    weather API testing

---
+++ test/integration test/routes/weather.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+let server;
+
+describe('/api/weather', () => {
+    beforeEach(() => { server = require('../../../index') });
+    afterEach(() => { server.close() });
+
+    describe('GET /', () => {
+        it('should return 400 no city provid', async () => {
+            const res = await request(server).get('/api/weather');
+            expect(res.status).toBe(400);
+        });
+    });
+
+    describe('GET /', () => {
+        it('should return weather data', async () => {
+            const res = await request(server).get('/api/weather').send({ city: 'Lagos' });
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('name', 'Lagos');
+        });
+    });
+});
