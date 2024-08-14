1392374163787af9b613953c29ecd47b95d644ea
Elksass315 <qsasomar@gmail.com>
2024-08-15 01:34:47 +0300

    update auth to sequelize

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    let user = await User.findOne({ email: req.body.email });
+    let user = await User.findOne({where: { email: req.body.email } });
     if (!user) return res.status(400).send('Invalid email or password.');
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
